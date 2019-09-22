e03bc19bb1338df81c73e8d8fe3fb5b411078d4e
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-22 01:29:38 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait?
+
+  def clickbait?
+    phrases = ["Won't Believe", "Secret", "Guess"]
+    if !title_contains_one_of_these_phrases?(title, phrases) && (title =~/(Top )[\d]/) == nil
+      error_message = "must contain one of these phrases: \"" + phrases.join("\", \"")
+      errors.add(:title, error_message + ", or Top [number]")
+    end
+  end
+
+  def title_contains_one_of_these_phrases?(title, phrases)
+    binding.pry
+    phrases.each {|phrase| return true if title.include?(phrase)}
+    false
+  end
 end
